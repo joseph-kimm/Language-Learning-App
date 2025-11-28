880ced30ed0734478ece3a2383e9d6d5a09b1827
Joseph Kim <josephjiminkim@gmail.com>
2025-11-28 14:54:59 +0000

    finished tutorial

--- nextjs-dashboard/app/ui/invoices/create-form.tsx
+++ nextjs-dashboard/app/ui/invoices/create-form.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { CustomerField } from '@/app/lib/definitions';
 import Link from 'next/link';
 import {
@@ -7,9 +9,12 @@ import {
   UserCircleIcon,
 } from '@heroicons/react/24/outline';
 import { Button } from '@/app/ui/button';
-import { createInvoice } from '@/app/lib/actions';
+import { createInvoice, State} from '@/app/lib/actions';
 
 export default function Form({ customers }: { customers: CustomerField[] }) {
+
+  const initialState: State = {message:null, errors: {}}
+
   return (
     <form action={createInvoice}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
